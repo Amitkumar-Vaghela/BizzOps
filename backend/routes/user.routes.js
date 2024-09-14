7510fbfc76a65226cf5959b5c070d16315dd00f3
ShyamKadiwar <shyamkadiwar!72gmsol.com>
2024-09-14 21:23:43 +0530

    add routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { changePassword, getCurrentUserDetails, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -9,5 +9,9 @@ router.route('/login').post(loginUser)
 
 // secured routes
 router.route('/logout').post(verifyJWT,logoutUser)
+router.route('/refresh-token').post(refreshAccessToken)
+router.route('/change-password').post(verifyJWT,changePassword)
+router.route('/get-details').get(verifyJWT,getCurrentUserDetails)
+router.route('/update-account').post(verifyJWT,updateAccountDetails)
 
 export default router
