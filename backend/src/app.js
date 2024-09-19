f004ff99d2a18de1a242cedfc8706d21416c37b5
ShyamKadiwar <shyamkadiwar!72gmsol.com>
2024-09-19 11:18:58 +0530

    add routes in app

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,11 +22,13 @@ import inventoryRouter from "../routes/inventory.routes.js"
 import salesRouter from "../routes/sales.routes.js"
 import customerRouter from "../routes/customer.routes.js"
 import invoiceRouter from "../routes/invoice.routes.js"
+import staffRouter from '../routes/staff.routes.js'
 //routes declaration
 app.use("/api/v1/users",userRouter) // this will become prefix like .../api/v1/users/register
 app.use("/api/v1/inventory",inventoryRouter)
 app.use("/api/v1/sales",salesRouter) 
 app.use("/api/v1/customer",customerRouter)
 app.use("/api/v1/invoice",invoiceRouter)
+app.use("/api/v1/staff",staffRouter)
 
 export {app}
