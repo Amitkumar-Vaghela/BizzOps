fc7a39eafd9600633d5ee7c06fc5f5d02b3118b1
ShyamKadiwar <shyamkadiwar!72gmsol.com>
2024-09-19 10:18:26 +0530

    add invoice routes

--- backend/routes/invoice.routes.js
+++ backend/routes/invoice.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addInvoice } from "../controllers/invoice.controller.js";
+import { addInvoice, getInvoice, getPaidInvoices, getUnpaidInvoices } from "../controllers/invoice.controller.js";
 
 
 const router = Router()
 
 router.route('/add-invoice').post(verifyJWT,addInvoice)
+router.route('/get-invoice').get(verifyJWT,getInvoice)
+router.route('/paid-invoice').get(verifyJWT,getPaidInvoices)
+router.route('/unpaid-invoice').get(verifyJWT,getUnpaidInvoices)
 
 export default router
