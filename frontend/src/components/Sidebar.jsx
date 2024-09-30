1bf6fb33d3a67022036d3c8c9c4021d2614a123f
Shyamkadiwar <shyamkadiwar17@gmail.com>
2024-09-30 17:44:46 +0530

    modified sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,27 +1,58 @@
 import React from "react";
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChartLine, faClockFour, faDollar, faFileInvoice, faMoneyBill, faNoteSticky, faReceipt, faShop, faUser, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { 
+    faChartLine, 
+    faClockFour, 
+    faDollar, 
+    faFileInvoice, 
+    faMoneyBill, 
+    faNoteSticky, 
+    faReceipt, 
+    faShop, 
+    faUser, 
+    faUsers 
+} from "@fortawesome/free-solid-svg-icons";
 
-function Sidebar(){
-    return(
-        <>
-            <div className="w-1/6 h-[calc(100vh)] sticky top-0 left-0 bg-white">
-                <img className="pt-10 ml-6 mb-10 w-40 h-20" src={logo} alt="log" />
-                <h1 className="font-font4 font-medium text text-md ml-7 mt-7"> <FontAwesomeIcon icon={faChartLine} className="text-blue-500 pr-2" /> Dashboard</h1>
-                <h1 className="font-font4 font-medium text text-md ml-7 mt-7"> <FontAwesomeIcon icon={faShop} className="text-blue-500 pr-2" /> Inventory</h1>
-                <h1 className="font-font4 font-medium text text-md ml-7 mt-7"> <FontAwesomeIcon icon={faMoneyBill} className="text-blue-500 pr-2" /> Sales</h1>
-                <h1 className="font-font4 font-medium text text-md ml-7 mt-7"> <FontAwesomeIcon icon={faFileInvoice} className="text-blue-500 pr-2" /> Invoices</h1>
-                <h1 className="font-font4 font-medium text text-md ml-7 mt-7"> <FontAwesomeIcon icon={faDollar} className="text-blue-500 pr-2" /> Expenses</h1>
-                <h1 className="font-font4 font-medium text text-md ml-7 mt-7"> <FontAwesomeIcon icon={faMoneyBill} className="text-blue-500 pr-2" /> Payment</h1>
-                <h1 className="font-font4 font-medium text text-md ml-7 mt-7"> <FontAwesomeIcon icon={faReceipt} className="text-blue-500 pr-2" /> Report</h1>
-                <h1 className="font-font4 font-medium text text-md ml-7 mt-7"> <FontAwesomeIcon icon={faClockFour} className="text-blue-500 pr-2" /> Orders</h1>
-                <h1 className="font-font4 font-medium text text-md ml-7 mt-7"> <FontAwesomeIcon icon={faUser} className="text-blue-500 pr-2" /> Customer</h1>
-                <h1 className="font-font4 font-medium text text-md ml-7 mt-7"> <FontAwesomeIcon icon={faUsers} className="text-blue-500 pr-2" /> Staff</h1>
-                <h1 className="font-font4 font-medium text text-md ml-7 mt-7 mb-2"> <FontAwesomeIcon icon={faNoteSticky} className="text-blue-500 pr-2" /> Notes</h1>
-            </div>
-        </>
-    )
+function Sidebar() {
+    return (
+        <div className="w-1/6 h-[calc(100vh)] sticky top-0 left-0 bg-white">
+            <img className="pt-10 ml-6 mb-10 w-40 h-20" src={logo} alt="logo" />
+            <h1 className="font-font4 font-medium text-md ml-7 mt-7">
+                <FontAwesomeIcon icon={faChartLine} className="text-blue-600 pr-2" /> Dashboard
+            </h1>
+            <h1 className="font-font4 font-medium text-md ml-7 mt-7">
+                <FontAwesomeIcon icon={faShop} className="text-green-600 pr-2" /> Inventory
+            </h1>
+            <h1 className="font-font4 font-medium text-md ml-7 mt-7">
+                <FontAwesomeIcon icon={faMoneyBill} className="text-yellow-500 pr-2" /> Sales
+            </h1>
+            <h1 className="font-font4 font-medium text-md ml-7 mt-7">
+                <FontAwesomeIcon icon={faFileInvoice} className="text-purple-500 pr-2" /> Invoices
+            </h1>
+            <h1 className="font-font4 font-medium text-md ml-7 mt-7">
+                <FontAwesomeIcon icon={faDollar} className="text-red-500 pr-2" /> Expenses
+            </h1>
+            <h1 className="font-font4 font-medium text-md ml-7 mt-7">
+                <FontAwesomeIcon icon={faMoneyBill} className="text-teal-500 pr-2" /> Payment
+            </h1>
+            <h1 className="font-font4 font-medium text-md ml-7 mt-7">
+                <FontAwesomeIcon icon={faReceipt} className="text-orange-500 pr-2" /> Report
+            </h1>
+            <h1 className="font-font4 font-medium text-md ml-7 mt-7">
+                <FontAwesomeIcon icon={faClockFour} className="text-indigo-500 pr-2" /> Orders
+            </h1>
+            <h1 className="font-font4 font-medium text-md ml-7 mt-7">
+                <FontAwesomeIcon icon={faUser} className="text-pink-500 pr-2" /> Customer
+            </h1>
+            <h1 className="font-font4 font-medium text-md ml-7 mt-7">
+                <FontAwesomeIcon icon={faUsers} className="text-gray-600 pr-2" /> Staff
+            </h1>
+            <h1 className="font-font4 font-medium text-md ml-7 mt-7 mb-2">
+                <FontAwesomeIcon icon={faNoteSticky} className="text-lime-500 pr-2" /> Notes
+            </h1>
+        </div>
+    );
 }
 
-export default Sidebar
+export default Sidebar;
