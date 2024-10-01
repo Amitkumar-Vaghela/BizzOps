00544fc55bd0066f9e96a63a423dba4689fa04bd
Shyamkadiwar <shyamkadiwar17@gmail.com>
2024-10-01 21:51:30 +0530

    added delete controller to inventory

--- backend/controllers/inventory.controller.js
+++ backend/controllers/inventory.controller.js
@@ -102,10 +102,37 @@ const removeStock = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, inventoryItem, "Stock updated successfully"));
 });
 
+const deleteInventoryItem = asyncHandler(async (req, res) => {
+    const { product } = req.body;  // product is the id of the inventory item
+    const owner = req.user?._id;
+
+    if (!owner) {
+        throw new ApiError(400, "User not found");
+    }
+
+    if (!product) {
+        throw new ApiError(400, "Product ID is required");
+    }
+
+    const inventoryItem = await Inventory.deleteOne({ _id: product, owner });
+
+    // if (!inventoryItem) {
+    //     throw new ApiError(404, "Inventory item not found or you do not own this item");
+    // }
+
+    // await inventoryItem.remove();
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, null, "Inventory item deleted successfully"));
+});
+
+
 
 export {
     addInventoryItem,
     getInventoryItem,
     addStock,
-    removeStock
+    removeStock,
+    deleteInventoryItem 
 }
--- backend/routes/inventory.routes.js
+++ backend/routes/inventory.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addInventoryItem, addStock, getInventoryItem, removeStock } from "../controllers/inventory.controller.js";
+import { addInventoryItem, addStock, deleteInventoryItem, getInventoryItem, removeStock } from "../controllers/inventory.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -8,5 +8,6 @@ router.route('/add-item').post(verifyJWT, addInventoryItem)
 router.route('/get-item').get(verifyJWT, getInventoryItem)
 router.route('/add-stock').post(verifyJWT, addStock)
 router.route('/remove-stock').post(verifyJWT, removeStock)
+router.route('/delete-item').post(verifyJWT, deleteInventoryItem)
 
 export default router
