52e9509cd2ab0ef28faad164b315719eadc4cfdd
Shyamkadiwar <shyamkadiwar17@gmail.com>
2024-10-05 21:28:34 +0530

    fix bugs in dashboard

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -12,36 +12,36 @@ import axios from "axios";
 
 function Dashboard() {
 
-    const [totalSale,setTotalSales]  = useState(0)
-    const [todaySale,setTodaySales]  = useState(0)
-    const [MonthSale,setMonthSales]  = useState(0)
-    const [totalProfit,setTotalProfit]  = useState(0)
-    const [todayProfit,setTodayProfit]  = useState(0)
-    const [monthProfit,setMonthProfit]  = useState(0)
-    const [totalCost,setTotalCost]  = useState(0)
-    const [totalNetIncome,setTotalNetIncome]  = useState(0)
-    const [totalExpense,setTotalExpense]  = useState(0)
-    const [orders, setOrders]  = useState(0)
-    const [pendingOrders, setPendingOrders]  = useState(0)
-    const [invoices, setInvoices]  = useState(0)
-    const [unpaidInvoices, setUnpaidInvoices]  = useState(0)
-    const [customers, setCustomers]  = useState(0)
+    const [totalSale, setTotalSales] = useState(0)
+    const [todaySale, setTodaySales] = useState(0)
+    const [MonthSale, setMonthSales] = useState(0)
+    const [totalProfit, setTotalProfit] = useState(0)
+    const [todayProfit, setTodayProfit] = useState(0)
+    const [monthProfit, setMonthProfit] = useState(0)
+    const [totalCost, setTotalCost] = useState(0)
+    const [totalNetIncome, setTotalNetIncome] = useState(0)
+    const [totalExpense, setTotalExpense] = useState(0)
+    const [orders, setOrders] = useState(0)
+    const [pendingOrders, setPendingOrders] = useState(0)
+    const [invoices, setInvoices] = useState(0)
+    const [unpaidInvoices, setUnpaidInvoices] = useState(0)
+    const [customers, setCustomers] = useState(0)
 
-    const fetchData = async() => {
+    const fetchData = async () => {
         try {
-            const totalSaleResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-alltime-sale",{withCredentials:true})
-            const totalProfitResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-alltime-profit",{withCredentials:true})
-            const totalCostResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-alltime-cost",{withCredentials:true})
-            const totalExpenseResponse = await axios.get("http://localhost:8000/api/v1/expense/get-alltime-expense",{withCredentials:true})
-            const todaySaleResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-oneday-sale",{withCredentials:true})
-            const todayProfitResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-one-profit",{withCredentials:true})
-            const monthSaleResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-last30Day-sale",{withCredentials:true})
-            const monthProfitResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-last30Day-profit",{withCredentials:true})
-            const orderResponse = await axios.get("http://localhost:8000/api/v1/orders/count-order",{withCredentials:true})
-            const pendingOrderResponse = await axios.get("http://localhost:8000/api/v1/orders/get-pending-order",{withCredentials:true})
-            const invoicesResponse = await axios.get("http://localhost:8000/api/v1/invoice/count-invoice",{withCredentials:true})
-            const unpaidInvoicesResponse = await axios.get("http://localhost:8000/api/v1/invoice/unpaid-invoice",{withCredentials:true})
-            const customersResponse = await axios.get("http://localhost:8000/api/v1/customer/count-customer",{withCredentials:true})
+            const totalSaleResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-alltime-sale", { withCredentials: true })
+            const totalProfitResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-alltime-profit", { withCredentials: true })
+            const totalCostResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-alltime-cost", { withCredentials: true })
+            const totalExpenseResponse = await axios.get("http://localhost:8000/api/v1/expense/get-alltime-expense", { withCredentials: true })
+            const todaySaleResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-oneday-sale", { withCredentials: true })
+            const todayProfitResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-one-profit", { withCredentials: true })
+            const monthSaleResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-last30Day-sale", { withCredentials: true })
+            const monthProfitResponse = await axios.get("http://localhost:8000/api/v1/sales/get-total-last30Day-profit", { withCredentials: true })
+            const orderResponse = await axios.get("http://localhost:8000/api/v1/orders/count-order", { withCredentials: true })
+            const pendingOrderResponse = await axios.get("http://localhost:8000/api/v1/orders/get-pending-order", { withCredentials: true })
+            const invoicesResponse = await axios.get("http://localhost:8000/api/v1/invoice/count-invoice", { withCredentials: true })
+            const unpaidInvoicesResponse = await axios.get("http://localhost:8000/api/v1/invoice/unpaid-invoice", { withCredentials: true })
+            const customersResponse = await axios.get("http://localhost:8000/api/v1/customer/count-customer", { withCredentials: true })
 
             setTotalSales(totalSaleResponse.data.data.totalSalesValue)
             setMonthSales(monthSaleResponse.data.data.totalSalesValue)
@@ -51,7 +51,7 @@ function Dashboard() {
             setTodayProfit(todayProfitResponse.data.data.totalProfitValue)
             setTotalCost(totalCostResponse.data.data.totalCostValue)
             setTotalExpense(totalExpenseResponse.data.data.totalExpenseValue)
-            setTotalNetIncome(totalProfit-totalExpense)
+            setTotalNetIncome(totalProfit - totalExpense)
             setOrders(orderResponse.data.data.totalOrders)
             setPendingOrders(pendingOrderResponse.data.data.pendingCount)
             setInvoices(invoicesResponse.data.data.invoiceCount)
@@ -59,14 +59,14 @@ function Dashboard() {
             setCustomers(customersResponse.data.data.count)
 
         } catch (error) {
-            console.error("Error while fetching data: ",error)
+            console.error("Error while fetching data: ", error)
         }
 
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchData();
-    },[])
+    }, [])
 
     return (
         <>
@@ -95,7 +95,11 @@ function Dashboard() {
                             <div className="bg-white shadow-lg w-full h-24 rounded-2xl flex flex-col items-center">
                                 <div className="w-full bg-white shadow-lg h-16  rounded-t-xl">
                                     <p className="text-base font-light font-font4 mt-1 ml-2"> <FontAwesomeIcon icon={faMoneyBill1} className="text-sm pr-1 text-teal-400" /> Total Profit</p>
-                                    <h1 className="text-black mb-1 text-2xl font-medium font-font4 ml-2">₹ {totalProfit.toLocaleString()}<samp className="font-font4 text-base">.00</samp></h1>
+                                    <h1 className="text-black mb-1 text-2xl font-medium font-font4 ml-2">
+                                        ₹ {totalProfit.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })}
+                                        <samp className="font-font4 text-base">.00</samp>
+                                    </h1>
+
                                     <hr />
                                 </div>
                                 <div className="w-11/12 mt-1 ml-">
@@ -108,7 +112,9 @@ function Dashboard() {
                             <div className="bg-white shadow-lg w-full h-24 rounded-xl flex flex-col items-center">
                                 <div className="w-full bg-white shadow-lg h-16  rounded-t-xl">
                                     <p className="text-base font-light font-font4 mt-1 ml-2"> <FontAwesomeIcon icon={faDollar} className="text-red-400 text-sm pr-2" /> Total Cost</p>
-                                    <h1 className="text-black text-2xl mb-1 font-medium font-font4 ml-2">₹ {totalCost.toLocaleString()}<samp className="font-font4 text-base">.00</samp></h1>
+                                    <h1 className="text-black text-2xl mb-1 font-medium font-font4 ml-2">
+                                    ₹ {totalCost.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })}
+                                        <samp className="font-font4 text-base">.00</samp></h1>
                                     <hr />
                                 </div>
                                 <div className="w-11/12 mt-1 ml-">
@@ -121,7 +127,7 @@ function Dashboard() {
                             <div className="bg-white shadow-lg w-full h-24 rounded-xl flex flex-col items-center">
                                 <div className="w-full bg-white shadow-lg h-16  rounded-t-xl">
                                     <p className="text-base font-light font-font4 mt-1 ml-2"> <FontAwesomeIcon icon={faWallet} className="text-xs text-green-400 pr-1" /> Total Net Income</p>
-                                    <h1 className="text-black text-2xl mb-1 font-medium font-font4 ml-2">₹ {totalProfit-totalExpense}<samp className="font-font4 text-base">.00</samp></h1>
+                                    <h1 className="text-black text-2xl mb-1 font-medium font-font4 ml-2">₹ {totalProfit - totalExpense}</h1>
                                     <hr />
                                 </div>
                                 <div className="w-11/12 mt-1 ml-">
@@ -183,7 +189,9 @@ function Dashboard() {
                                 <div className="bg-blue-200  shadow-lg w-full h-24 rounded-2xl flex flex-col items-center">
                                     <div className="w-full bg-blue-50 shadow-lg h-16  rounded-t-xl">
                                         <p className="text-base font-light font-font4 mt-1 ml-2">Profit</p>
-                                    <h1 className="text-black mb-1 text-2xl font-medium font-font4 ml-2">₹{todayProfit.toLocaleString()}<samp className="font-font4 text-base">.00</samp></h1>
+                                        <h1 className="text-black mb-1 text-2xl font-medium font-font4 ml-2">
+                                        ₹ {todayProfit.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })}
+                                            <samp className="font-font4 text-base">.00</samp></h1>
                                         <hr />
                                     </div>
                                     <div className="w-11/12 mt-1 ml-">
@@ -192,10 +200,12 @@ function Dashboard() {
                                 </div>
                             </div>
                             <div className="h-full mt-10">
-                            <div className="bg-blue-200  shadow-lg w-full h-24 rounded-2xl flex flex-col items-center">
+                                <div className="bg-blue-200  shadow-lg w-full h-24 rounded-2xl flex flex-col items-center">
                                     <div className="w-full bg-blue-50 shadow-lg h-16  rounded-t-xl">
                                         <p className="text-base font-light font-font4 mt-1 ml-2">Profit</p>
-                                        <h1 className="text-black mb-1 text-2xl font-medium font-font4 ml-2">₹ {monthProfit.toLocaleString()}<samp className="font-font4 text-base">.00</samp></h1>
+                                        <h1 className="text-black mb-1 text-2xl font-medium font-font4 ml-2">
+                                        ₹ {monthProfit.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })}
+                                            <samp className="font-font4 text-base">.00</samp></h1>
                                         <hr />
                                     </div>
                                     <div className="w-11/12 mt-1 ml-">
@@ -238,7 +248,9 @@ function Dashboard() {
                                 <div className="w-2/4 bg-blue-50 rounded-3xl text-center shadow-md">
                                     <p className="font-font4 font-normal m-2 text-xs">Unpaid Invoices</p>
                                     <hr />
-                                    <h1 className="font-font4 font-semibold text-md m-2">₹ {unpaidInvoices.toLocaleString() || 0}</h1>
+                                    <h1 className="font-font4 font-semibold text-md m-2">
+                                    ₹ {unpaidInvoices.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 }) || 0}
+                                    </h1>
                                 </div>
                             </div>
                             <div className="w-full flex justify-center gap-4 mt-4 h-24">
@@ -257,14 +269,14 @@ function Dashboard() {
                         <div className="w-2/4 mt-4 bg-blue-50 shadow-lg rounded-3xl">
                             <h1 className="text-center font-medium text-sm mb-3 font-font4 mt-4">Sales v/s Profit v/s Cost v/s Expenses</h1>
                             <div className=" mr-3">
-                            <FinancialChart />
+                                <FinancialChart />
                             </div>
                         </div>
 
                         <div className="w-1/4 mt-4 bg-blue-50 shadow-lg rounded-3xl">
                             <h1 className="text-center font-medium text-sm font-font4 mt-4">Financial Distribution</h1>
                             <div className=" mr-3">
-                            <FinancialDist />
+                                <FinancialDist />
                             </div>
                         </div>
 
