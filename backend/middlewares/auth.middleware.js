dc78215d3f7d1dab7e344998f13ab803413b51d7
Shyamkadiwar <shyamkadiwar17@gmail.com>
2024-10-17 11:40:02 +0530

    modified auth middelware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,30 +1,29 @@
-import { asyncHandler } from '../utils/asyncHandler.js';
-import { ApiError } from '../utils/ApiError.js';
-import { User } from '../models/user.model.js';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import {asyncHandler} from '../utils/asyncHandler.js'
+import {ApiError} from '../utils/ApiError.js'
+import {User} from '../models/user.model.js'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const verifyJWT = asyncHandler(async (req, res, next) => {
+const verifyJWT = asyncHandler(async(req, res, next)=>{
     try {
-        const token = req.header('Authorization')?.replace("Bearer ", ""); // Get the token from the Authorization header
-        if (!token) {
-            throw new ApiError(401, "Unauthorized request: No token provided");
+        const token = req.cookies?.accessToken || req.header('Authorization')?.replace("Bearer ","")
+        if(!token){
+            throw new ApiError(401,"Unauthorized request")
         }
-
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        const user = await User.findById(decodedToken._id).select("-password -refreshToken");
-        if (!user) {
-            throw new ApiError(401, "Invalid accessToken");
+    
+        const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decodedToken._id).select("-password -refreshToken")
+        if(!user){
+            throw new ApiError(401,"Invalid accessToken")
         }
-
+        
         req.user = user;
-        next();
+        next()
     } catch (error) {
-        throw new ApiError(400, error?.message || "Invalid access token");
+        throw new ApiError(400, error?.message || "invalid access token")
     }
-});
+})
 
-
-export { verifyJWT };
+export {verifyJWT}
