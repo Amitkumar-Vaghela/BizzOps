26e31ae08b0ca0095955cb5740b966f8e01cd86b
Shyamkadiwar <shyamkadiwar17@gmail.com>
2024-10-17 11:33:17 +0530

    modified auth middelware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -8,28 +8,23 @@ dotenv.config();
 
 const verifyJWT = asyncHandler(async (req, res, next) => {
     try {
-        const token = req.cookies?.accessToken || req.header('Authorization')?.replace("Bearer ", "");
-        
+        const token = req.header('Authorization')?.replace("Bearer ", ""); // Get the token from the Authorization header
         if (!token) {
             throw new ApiError(401, "Unauthorized request: No token provided");
         }
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         const user = await User.findById(decodedToken._id).select("-password -refreshToken");
-
         if (!user) {
-            throw new ApiError(401, "Unauthorized request: Invalid access token");
+            throw new ApiError(401, "Invalid accessToken");
         }
 
         req.user = user;
         next();
     } catch (error) {
-        const message = error.message.includes("invalid signature") || error.message.includes("jwt expired")
-            ? "Unauthorized request: Invalid or expired access token"
-            : "Unauthorized request: " + (error.message || "Invalid access token");
-
-        throw new ApiError(401, message);
+        throw new ApiError(400, error?.message || "Invalid access token");
     }
 });
+
 
 export { verifyJWT };
