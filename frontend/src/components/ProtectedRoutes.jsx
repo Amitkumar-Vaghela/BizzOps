b62167f4691c34941b7f92a4f8e02e2a433fc63b
Krish Soni <sonikrish994@gmail.com>
2025-04-11 17:07:29 +0530

    agent bootstrap

--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from './Context/AuthContext';
 
+// eslint-disable-next-line react/prop-types
 const ProtectedRoute = ({ element }) => {
   const { isAuthenticated } = useAuth();
 
