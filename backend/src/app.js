97311d69349675a6db36e44cfc971709fa04c886
Shyamkadiwar <shyamkadiwar17@gmail.com>
2025-06-19 11:16:28 +0530

    modified cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,22 +8,6 @@ dotenv.config();
 
 const app = express();
 
-// CORS configuration
-// const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : [];
-// const allowedOrigins = process.env.CORS_ORIGIN
-//   ? process.env.CORS_ORIGIN.split(',').map(origin => origin.trim())
-//   : [];
-// app.use(cors({
-//     origin: function (origin, callback) {
-//         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     credentials: true
-// }));
-
 const allowedOrigins = process.env.CORS_ORIGIN
   ? process.env.CORS_ORIGIN.split(',').map(origin => origin.trim())
   : [];
