bc95cced942f61ede7269212719fc881390ab2c9
Shyamkadiwar <shyamkadiwar17@gmail.com>
2025-07-28 12:18:21 +0530

    modified backend url add auth with  cookies

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,5 +1,3 @@
-// middlewares/verifyJWT.js
-
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { ApiError } from '../utils/ApiError.js';
 import { User } from '../models/user.model.js';
